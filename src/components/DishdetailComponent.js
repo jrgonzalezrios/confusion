e7162f9a86016d753b10645a14fdac285a9001e5
Julio Gonzalez <jrgonzalezrios@gmail.com>
2019-07-04 05:55:58 -0600

    Components part 2

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class DishdetailComponent extends Component {
+    
+}
+
+export default DishdetailComponent;
