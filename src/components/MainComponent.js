3aab373280c3c222340aebf4a0078b4d72b991c5
Julio Gonzalez <jrgonzalezrios@gmail.com>
2019-07-15 19:32:44 -0600

    Using React Router for navitation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,8 @@ import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import { DISHES } from '../shared/dishes';
 
@@ -10,24 +12,27 @@ class Main extends Component {
 
   constructor(props){
     super(props);
+
     this.state = {
-      dishes : DISHES,
-      selectedDish: null
+      dishes : DISHES,      
     }
   }
 
-  onDishSelect(dish){
-    this.setState({ selectedDish: dish })
-  }
 
   render(){
+    const HomePage = () =>{
+      return(
+        <Home />
+      )
+    }
     return (
       <div>
-        <Header />    
-       <Menu dishes = {this.state.dishes} 
-          onClick={(dishId)=> this.onDishSelect(dishId)}/>
-       <DishDetail 
-          dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}/>
+        <Header />
+       <Switch>
+         <Route path="/home" component={HomePage} />
+         <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+         <Redirect to="/home" />
+       </Switch>
        <Footer />
       </div>
     );
